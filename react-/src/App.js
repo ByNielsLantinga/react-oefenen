ac72dd04cd0e687107ddadef21bd1a1cf34659c2
Niels <i514890@fhict.nl>
2023-12-12 15:38:38 +0100

    JSX gebruikt in "Help.js"

--- react-/src/App.js
+++ react-/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hello from "./components/Hello";
 import Message from "./components/Message";
+import Help from "./components/Help";
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <Help/>
       <Message/>
     </div>
   );
---
+++ react-/src/components/Help.js
@@ -0,0 +1,9 @@
+const displayMessage = ()=> {
+    return "I need help"
+}
+
+function Help(){
+    return <h1>The message is: {displayMessage()}</h1>
+}
+
+export default Help
